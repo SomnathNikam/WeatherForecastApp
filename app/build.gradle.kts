a0f14c1ddf3fd4d1ead857ce299d7116e05781b9
SOMNATH <somnathnikam2076@gmail.com>
2025-01-25 09:49:10 +0530

    Second Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    // Retrofit Dependency
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // Dexter Runtime Library
+    implementation("com.karumi:dexter:6.2.3")
+
+    // This is for the location
+    implementation("com.google.android.gms:play-services-location:21.3.0")
 }
